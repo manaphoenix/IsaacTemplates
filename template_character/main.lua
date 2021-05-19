c5ad8fec5ca991985da8c4d292e980336ab380bd
manaphoenix <toehead976@gmail.com>
2021-05-18 22:46:26 -0400

    Added support for tainted characters to have its own stats/items

--- template_character/main.lua
+++ template_character/main.lua
@@ -1,17 +1,30 @@
 local mod = RegisterMod("MODNAME", 1) -- Replace the Mod name with something unique.
 local playerName = "Alpha" -- the name of your character (from the players.xml file)
-local isTainted = false -- is this a tainted character?
+local taintedName = "Omega" -- set this to nil if your tainted character doens't use a different name
 
 local stats = {
-  damage = 1.00,
-  firedelay = 1.00,
-  shotspeed = 1.00,
-  range = 1.00,
-  speed = 1.00,
-  tearflags = TearFlags.TEAR_POISON | TearFlags.TEAR_FREEZE,
-  tearcolor = Color(1.0, 1.0, 1.0, 1.0, 255, 255, 255),
-  flying = false,
-  luck = 1.00
+  default = {
+    damage = 2.00,
+    firedelay = 1.00,
+    shotspeed = 1.00,
+    range = 1.00,
+    speed = 1.00,
+    tearflags = TearFlags.TEAR_POISON | TearFlags.TEAR_FREEZE,
+    tearcolor = Color(1.0, 1.0, 1.0, 1.0, 255, 255, 255),
+    flying = false,
+    luck = 1.00
+  },
+  tainted = {
+    damage = 5.00,
+    firedelay = 1.00,
+    shotspeed = 1.00,
+    range = 1.00,
+    speed = 1.00,
+    tearflags = TearFlags.TEAR_POISON | TearFlags.TEAR_FREEZE,
+    tearcolor = Color(1.0, 1.0, 1.0, 1.0, 255, 255, 255),
+    flying = false,
+    luck = 1.00
+  }
 }
 --[[
 The way stats work all characters start with isaac's base.
@@ -24,12 +37,29 @@ if you set damage to 1.00, then your character will start with 3.50 damage (the 
 set tearflags to TearFlags.TEAR_NORMAL if you don't want your character to have any innate tear effects
 ]]
 local items = {
-  -- item1,
-  -- item2
+  default = {
+    --item1,
+    --item2
+  },
+  tainted = {
+    --item1,
+    --item2
+  }
 }
-local trinket = TrinketType.TRINKET_SWALLOWED_PENNY
-local card = Card.CARD_FOOL
-local pill = 0
+
+local trinket = {
+  default = TrinketType.TRINKET_SWALLOWED_PENNY,
+  tainted = TrinketType.TRINKET_SWALLOWED_PENNY
+}
+
+local card = {
+  default = Card.CARD_FOOL,
+  tainted = Card.CARD_FOOL
+}
+local pill = {
+  default = 0,
+  tainted = 0
+}
 --[[
 Fill in the above with the items you want your character to start with.
 separated by a comma (like above, only not commented out using the '--')
@@ -65,70 +95,135 @@ the last number is the ID
 
 -- CODE --
 
-local char = Isaac.GetPlayerTypeByName(playerName, isTainted)
+local char = Isaac.GetPlayerTypeByName(playerName, false)
+local taintedChar = Isaac.GetPlayerTypeByName(taintedName or playerName, true)
 local game = Game()
+local function IsTainted(player)
+  if (player:GetPlayerType() ~= char and player:GetPlayerType() ~= taintedChar) then return nil end
+
+  if player:GetPlayerType() == char then return false end
+
+  return true
+end
+
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player, cache)
-  if (player:GetPlayerType() ~= char) then return end
+  if (player:GetPlayerType() ~= char and player:GetPlayerType() ~= taintedChar) then return end
+  local taint = IsTainted(player)
 
   if (cache & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE) then
-    player.Damage = player.Damage * stats.damage
+    if (not taint) then
+      player.Damage = player.Damage * stats.default.damage
+    else
+      player.Damage = player.Damage * stats.tainted.damage
+    end
   end
 
   if (cache & CacheFlag.CACHE_FIREDELAY == CacheFlag.CACHE_FIREDELAY) then
-    player.MaxFireDelay = player.MaxFireDelay * stats.firedelay
+    if (not taint) then
+      player.MaxFireDelay = player.MaxFireDelay * stats.default.firedelay
+    else
+      player.MaxFireDelay = player.MaxFireDelay * stats.tainted.firedelay
+    end
   end
 
   if (cache & CacheFlag.CACHE_SHOTSPEED == CacheFlag.CACHE_SHOTSPEED) then
-    player.ShotSpeed = player.ShotSpeed * stats.shotspeed
+    if (not taint) then
+      player.ShotSpeed = player.ShotSpeed * stats.default.shotspeed
+    else
+      player.ShotSpeed = player.ShotSpeed * stats.tainted.shotspeed
+    end
   end
 
   if (cache & CacheFlag.CACHE_RANGE == CacheFlag.CACHE_RANGE) then
-    player.TearHeight = player.TearHeight * stats.range
+    if (not taint) then
+      player.TearHeight = player.TearHeight * stats.default.range
+    else
+      player.TearHeight = player.TearHeight * stats.tainted.range
+    end
   end
 
   if (cache & CacheFlag.CACHE_SPEED == CacheFlag.SPEED) then
-    player.MoveSpeed = player.MoveSpeed * stats.speed
+    if (not taint) then
+      player.MoveSpeed = player.MoveSpeed * stats.default.speed
+    else
+      player.MoveSpeed = player.MoveSpeed * stats.tainted.speed
+    end
   end
 
   if (cache & CacheFlag.CACHE_LUCK == CacheFlag.CACHE_LUCK) then
-    player.Luck = player.Luck * stats.luck
+    if (not taint) then
+      player.Luck = player.Luck * stats.default.luck
+    else
+      player.Luck = player.Luck * stats.tainted.luck
+    end
   end
 
   if (cache & CacheFlag.CACHE_FLYING == CacheFlag.CACHE_FLYING) then
-    player.CanFly = stats.flying
+    if (not taint) then
+      player.CanFly = stats.default.flying
+    else
+      player.CanFly = stats.tainted.flying
+    end
   end
 
   if (cache & CacheFlag.CACHE_TEARFLAG == CacheFlag.CACHE_TEARFLAG) then
-    player.TearFlags = player.TearFlags | stats.tearflags
+    if (not taint) then
+      player.TearFlags = player.TearFlags | stats.default.tearflags
+    else
+      player.TearFlags = player.TearFlags | stats.tainted.tearflags
+    end
   end
 
   if (cache & CacheFlag.CACHE_TEARCOLOR == CacheFlag.CACHE_TEARCOLOR) then
-    player.TearColor = stats.tearcolor
+    if (not taint) then
+      player.TearColor = stats.default.tearcolor
+    else
+      player.TearColor = stats.tainted.tearcolor
+    end
   end
 end)
 
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, function(_, player)
-  if (player:GetPlayerType() ~= char) then return end
+  if (player:GetPlayerType() ~= char and player:GetPlayerType() ~= taintedChar) then return end
   local TotPlayers = #Isaac.FindByType(EntityType.ENTITY_PLAYER)
+  local taint = IsTainted(player)
 
   if TotPlayers == 0 and game:GetFrameCount() == 0 then
-    if (#items > 0) then
-      for i,v in ipairs(items) do
-        player:AddCollectible(v)
+    if (#items.default > 0 or #items.tainted) then
+      if (not taint) then
+        for i,v in ipairs(items.default) do
+          player:AddCollectible(v)
+        end
+      else
+        for i,v in ipairs(items.tainted) do
+          player:AddCollectible(v)
+        end
       end
     end
 
-    if (REPENTANCE and trinket ~= 0) then
-      player:AddTrinket(trinket, true)
+    if (REPENTANCE and ((trinket.default ~= 0 and not taint) or (trinket.tainted ~= 0 and taint))) then
+      if (not taint) then
+        player:AddTrinket(trinket.default, true)
+      else
+        player:AddTrinket(trinket.tainted, true)
+      end
     end
 
-    if pill ~= 0 then
-      player:SetPill(0, pill)
+    if (pill.default ~= 0 and not taint) or (pill.tainted ~= 0 and taint) then
+      if (not taint) then
+        player:SetPill(0, pill.default)
+      else
+        player:SetPill(0, pill.tainted)
+      end
     end
 
-    if card ~= 0 then
-      player:SetCard(0, card)
+    if (card.default ~= 0 and not taint) or (card.tainted ~= 0 and taint) then
+      if (not taint) then
+        player:SetCard(0, card.default)
+      else
+        player:SetCard(0, card.tainted)
+      end
     end
   end
 end)
