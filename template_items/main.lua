e67c5e1c8c4b8cd1a181a8de816c7152ad4da601
manaphoenix <toehead976@gmail.com>
2023-05-03 17:54:45 -0400

    corrected my dumb mistakes

--- template_items/main.lua
+++ template_items/main.lua
@@ -1,7 +1,7 @@
 local mod = RegisterMod("TemplateItems", 1)
 
-local itemRegistry = include("Registries.ItemRegistry")
-local eidRegistry = include("Registries.EIDRegistry")
+local itemRegistry = include("registry.ItemRegistry")
+local eidRegistry = include("registry.EIDRegistry")
 eidRegistry.register(itemRegistry) -- actually register the items
 
 local items = {                    -- list of file names in the items folder, so it can go through each one and run their callbacks
@@ -21,7 +21,7 @@ end
 -- go through each item and run its callbacks
 for i = 1, #items do
     ---@type ItemObject
-    local item = include(items[i])                     -- get the item file
+    local item = include("items." .. items[i])                     -- get the item file
     for c = 1, #item.callbacks do                      -- for each one of the callbacks
         local cb = item.callbacks[c]                   -- get the callback
         registerCallback(cb.callback, cb.func, cb.opt) -- register the callback
